7ff2ec542fd720cdb53c1ee8e3e42e61da497f5a
Ish Kumar <help.arinsharma+git@gmail.com>
2025-05-25 20:24:44 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,12 +1,12 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://graceful-banoffee-cdcb59.netlify.app",
 
   VITE_BACKEND_URL: "https://server.vidbinge.com",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzlkZWYyZDY5ZWFlNDk4ZjJiOTI4MTgyNDdjM2ViMCIsInN1YiI6IjY2MjdmMGJlNjJmMzM1MDE0YmQ4NTFmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h3KpPvkiaz8uNz1bntAKqsPrxG_4UUWaY3kYME6N6m8",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYzZlYmQ3YjM1ZWZiZDcyNmVmMDBmZjI1NjE0OWVjNiIsIm5iZiI6MTczMTA5NjY4NS4zNTUsInN1YiI6IjY3MmU3MDZkNGU0NTk5ZjI0ZTNkOTdkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sEwyD_NiJ7DK3uHTErUdSyZi9XLOig0Ypz8SrI8NdI8",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -15,7 +15,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: true,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: null,
+
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
